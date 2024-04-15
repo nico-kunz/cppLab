175833efcb40506f83a06060278faf8f0651d581
Nico Kunz <nicokunznk00@gmail.com>
2024-04-15 20:35:40 +0200

    Add main function for testing

--- 01_basics/src/main.cpp
+++ 01_basics/src/main.cpp
@@ -1,3 +1,14 @@
 //
 // Created by nicokunz on 4/15/24.
 //
+
+#include <iostream>
+#include "Complex.hpp"
+int main() {
+  Complex c = ~Complex(1,2);
+
+  std::cout << c << std::endl;
+
+  c = ~c;
+  std::cout << c << std::endl;
+}
