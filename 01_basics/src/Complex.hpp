586be3527ede1697c6e61d6ddd03e549323500c9
Nico Kunz <nicokunznk00@gmail.com>
2024-04-15 19:38:29 +0200

    Add member variables and functions to Complex class

--- 01_basics/src/Complex.hpp
+++ 01_basics/src/Complex.hpp
@@ -5,8 +5,20 @@
 #ifndef INC_01_BASICS_SRC_COMPLEX_HPP_
 #define INC_01_BASICS_SRC_COMPLEX_HPP_
 
+#include <string>
 class Complex {
+ public:
+  Complex();
+  Complex(double real, double imaginary);
+  static Complex from_euler(double abs, double angle);
+  double real() const;
+  double imag() const;
+  double abs() const;
+  double angle() const;
 
+ private:
+  double c_real;
+  double c_imaginary;
 };
 
 #endif //INC_01_BASICS_SRC_COMPLEX_HPP_
