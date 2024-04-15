c55558c508366b3d6aef41c17de75745a3a01e59
Nico Kunz <nicokunznk00@gmail.com>
2024-04-15 21:30:16 +0200

    Add more tests to main.cpp

--- 01_basics/src/main.cpp
+++ 01_basics/src/main.cpp
@@ -11,4 +11,21 @@ int main() {
 
   c = ~c;
   std::cout << c << std::endl;
+
+  Complex a = Complex(1,2);
+  Complex b = Complex(1,4);
+
+  c = a + b;
+  Complex d = a - b;
+  Complex e = a * b;
+  Complex f = a / b;
+  std::cout << c << std::endl;
+  std::cout << d << std::endl;
+  std::cout << e << std::endl;
+  std::cout << f << std::endl;
+
+  std::cout << c.angle() << std::endl;
+
+  std::cout << Complex::from_euler(3, 0.34906585) << std::endl;
+
 }
