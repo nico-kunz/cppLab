bbc8eb0c2259b4a3d33a4812a87d3666f7f85754
Nico Kunz <nicokunznk00@gmail.com>
2024-04-15 19:54:26 +0200

    Implement constructors and getters of Complex

--- 01_basics/src/Complex.cpp
+++ 01_basics/src/Complex.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "Complex.hpp"
+/**
+ * Creates Complex number with default values 0 + 0i
+ */
+Complex::Complex() {
+  c_real = 0;
+  c_imaginary = 0;
+}
+
+Complex::Complex(double real, double imaginary) {
+  c_real = real;
+  c_imaginary = imaginary;
+}
+
+double Complex::real() const {
+  return c_real;
+}
+
+double Complex::imag() const {
+  return c_imaginary;
+}
+
+
