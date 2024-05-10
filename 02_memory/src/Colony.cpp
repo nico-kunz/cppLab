55561dfc92922c0a6eadc4c4bafbd0ff065976fc
Nico Kunz <nicokunznk00@gmail.com>
2024-05-10 22:49:22 +0200

    Implement task 1

--- 02_memory/src/Colony.cpp
+++ 02_memory/src/Colony.cpp
@@ -2,3 +2,26 @@
 #include "Colony.hpp"
 
 // TODO: 2.1
+Colony::Colony(bool initial_state) {
+  state = initial_state;
+}
+
+bool Colony::getState() const {
+  return state;
+}
+
+void Colony::evolve() {
+  state = next_state;
+}
+
+void Colony::calculateNextState(int alive_neighbors) {
+  // if less than 2 or more than 3 neighbors, die because of over-/underpopulation
+  if (state && (alive_neighbors < 2 || alive_neighbors > 3)) {
+    next_state = false;
+  }
+
+  // Colony will be revived if it has 3 neighbors because of Expansion
+  if(!state && alive_neighbors == 3) {
+    next_state = true;
+  }
+}
