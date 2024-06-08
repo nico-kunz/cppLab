2f3336b1f26f143155481e4936505907809d4b01
Nico Kunz <nicokunznk00@gmail.com>
2024-06-08 03:10:33 +0200

    Implement constructor and print_result_map methods in Player

--- 03_oo/src/Player.cpp
+++ 03_oo/src/Player.cpp
@@ -3,4 +3,25 @@
 #include <iostream>
 
 void Player::print_result_map() {
+    for (int i = 0; i < m_attack_board.size(); ++i) {
+        std::cout << std::string(4 * m_attack_board.size() + 1, '-') << std::endl;
+        for (int j = 0; j < m_attack_board.size(); ++j) {
+            std::cout << "|";
+            switch (m_attack_board[j][i]) {
+                case AttackField::MISS: std::cout << " ~ ";
+                    break;
+                case AttackField::HIT: std::cout << " X ";
+                    break;
+                case AttackField::NONE:
+                    std::cout << "   ";
+                    break;
+            }
+        }
+        std::cout << "| " << i << std::endl;
+    }
+    std::cout << std::string(4 * m_attack_board.size() + 1, '-') << std::endl;
+
+    for (char c = 'A'; c < 'A' + m_attack_board.size(); c++) {
+        std::cout << "  " << c << " ";
+    }
 }
--- 03_oo/src/Player.hpp
+++ 03_oo/src/Player.hpp
@@ -5,13 +5,28 @@
 
 #include "Board.hpp"
 
+enum class AttackField {
+    HIT,
+    MISS,
+    NONE
+};
+
 class Player {
   public:
-    Player(ITargetBoard& target_board) {}
+    Player(ITargetBoard &target_board) : m_target_board(target_board),
+                                         m_attack_board(target_board.dim(),
+                                                        std::vector<AttackField>(target_board.dim(),
+                                                                                 AttackField::NONE)) {
 
-    virtual ~Player() {}
+    }
+
+    virtual ~Player() {
+    }
 
     virtual bool performNextMove() = 0;
 
     void print_result_map();
+  protected:
+    ITargetBoard &m_target_board;
+    std::vector<std::vector<AttackField>> m_attack_board;
 };
