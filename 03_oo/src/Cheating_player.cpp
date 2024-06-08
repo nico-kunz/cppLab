6ce83ba6bfe6a538cecb1cf2187af23dfa6ae25d
Nico Kunz <nicokunznk00@gmail.com>
2024-06-08 03:12:15 +0200

    Implement Cheating_player

--- 03_oo/src/Cheating_player.cpp
+++ 03_oo/src/Cheating_player.cpp
@@ -1,9 +1,20 @@
 #include "Cheating_player.hpp"
 
 #include <cassert>
+#include <iostream>
 
-CheatingPlayer::CheatingPlayer(ITargetBoard& target_board) : Player(target_board) {}
+CheatingPlayer::CheatingPlayer(ITargetBoard &target_board) : Player(target_board) {}
 
 bool CheatingPlayer::performNextMove() {
+    Board board = dynamic_cast<Board &>(m_target_board);
+    for (int i = 0; i < m_target_board.dim(); ++i) {
+        for (int j = 0; j < m_target_board.dim(); ++j) {
+            if (board.m_board[i][j] == Field::SHIP) {
+                m_target_board.fire('A' + i, j);
+                return true;
+            }
+        }
+    }
+    std::cout << "Something went wrong here..." << std::endl;
     return false;
 }
