b3e72b4eb4264f2d52db90a5c6eb9b0616eb9503
Nico Kunz <nicokunznk00@gmail.com>
2024-06-08 03:11:11 +0200

    Implement Random_player

--- 03_oo/src/Random_player.cpp
+++ 03_oo/src/Random_player.cpp
@@ -1,9 +1,29 @@
 #include "Random_player.hpp"
 
 #include <cstdlib>
+#include <iostream>
 
-RandomPlayer::RandomPlayer(ITargetBoard& target_board) : Player{ target_board } {}
+RandomPlayer::RandomPlayer(ITargetBoard &target_board) : Player{target_board} {}
 
 bool RandomPlayer::performNextMove() {
-    return false;
+    srand(rand() + time(nullptr));
+
+    int col = rand() % 10;
+    int row = rand() % 10;
+
+    // keep generating new random coords until field with no previous attack is found
+    while (m_attack_board[col][row] != AttackField::NONE) {
+        col = rand() % 10;
+        row = rand() % 10;
+    }
+
+    bool isHit = m_target_board.fire('A' + col, row);
+
+    if (isHit) {
+        m_attack_board[col][row] = AttackField::HIT;
+    } else {
+        m_attack_board[col][row] = AttackField::MISS;
+    }
+
+    return isHit;
 }
