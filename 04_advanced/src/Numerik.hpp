8745193a8601b224ec2a7a144d47974aeac2e0bb
Nico Kunz <nicokunznk00@gmail.com>
2024-06-23 02:03:35 +0200

    Implement lab 4 exercise 2

--- 04_advanced/src/Numerik.hpp
+++ 04_advanced/src/Numerik.hpp
@@ -2,17 +2,30 @@
 
 #include <cstddef>
 
-template <typename F>
-double trapz(F fx, double a, double b, size_t n) {
-    return 0.0;
+double trapz(double (*fx)(double), double a, double b, size_t n) {
+    double interval_len = (b-a)/n;
+    double sum = 0.0;
+
+    for(int i = 1; i < n; ++i) {
+        sum += fx(a + i*interval_len);
+    }
+
+    return interval_len * ((fx(b) + fx(a))/2 + sum);
 }
 
 template <typename T>
 T fxT(T x) {
-    return 0;
+    return x * x * x + 4*(x * x) - T{7}*x + T{12};
 }
 
-template <typename T, typename F>
-T trapzT(F fx, T a, T b, size_t n) {
-    return 0;
+template <typename T>
+T trapzT(T (*fx)(T), T a, T b, size_t n) {
+    T interval_len = (b-a)/n;
+    T sum = T{};
+
+    for(int i = 1; i < n; ++i) {
+        sum += fx(a + i*interval_len);
+    }
+
+    return interval_len * ((fx(b) + fx(a))/2 + sum);
 }
